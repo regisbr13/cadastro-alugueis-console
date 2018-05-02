fa997411e5b0defe52dfee8f725efc417f8d46bf
Regis Lima <regislima1391@gmail.com>
2018-05-02 14:34:14 -0300

    Implementado cadastro de hospedes

--- AlugueisConsole/Aluguel.cs
+++ AlugueisConsole/Aluguel.cs
@@ -1,12 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AlugueisConsole
 {
     class Aluguel
     {
+        public string Hospede { get; set; }
+        public string Email { get; set; }
+        public int Numero { get; set; }
+
+        public Aluguel(string hospede, string email, int numero)
+        {
+            this.Hospede = hospede;
+            this.Email = email;
+            this.Numero = numero;
+        }
     }
 }
--- AlugueisConsole/Program.cs
+++ AlugueisConsole/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AlugueisConsole
 {
@@ -10,6 +6,24 @@ namespace AlugueisConsole
     {
         static void Main(string[] args)
         {
+            Console.Write("Qual a quantidade de quartos disponíveis para alugar? ");
+            int qteQuartos = int.Parse(Console.ReadLine());
+            Aluguel[] alugueis = new Aluguel[qteQuartos];
+
+            Console.WriteLine();
+            Console.Write("Quantos aluguéis serão registrados? (1 - " + qteQuartos + ")");
+            int qteAlugueis = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= qteAlugueis; i++)
+            {
+                Console.WriteLine("Dados do " + i + "° aluguel:");
+                Console.Write("Nome: ");
+                string hospede = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int numeroQuarto = int.Parse(Console.ReadLine());
+                alugueis[i - 1] = new Aluguel(hospede, email, numeroQuarto);
+            }
         }
     }
 }

b12208ad5f78fc4e578b0ee5db060f9ffa6606b7
Regis Lima <regislima1391@gmail.com>
2018-05-14 15:19:07 -0300

    Tratamento de erros

--- AlugueisConsole/Program.cs
+++ AlugueisConsole/Program.cs
@@ -7,45 +7,63 @@ namespace AlugueisConsole
         //Aplicação simples de console para cadastro e listagem de alugueis:
         static void Main(string[] args)
         {
-            Console.Write("Qual a quantidade de quartos disponíveis para alugar? ");
-            int qteQuartos = int.Parse(Console.ReadLine());
-            Aluguel[] alugueis = new Aluguel[qteQuartos];
-
-            Console.WriteLine();
-            Console.Write("Quantos aluguéis serão registrados (1 a " + qteQuartos + ")? ");
-            int qteAlugueis = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= qteAlugueis; i++)
+            int k = 0;
+            while (k < 1)
             {
-                Console.WriteLine();
-                Console.WriteLine("Dados do " + i + "° aluguel:");
-                Console.Write("Nome: ");
-                string hospede = Console.ReadLine();
-                Console.Write("Email: ");
-                string email = Console.ReadLine();
-                Console.Write("Quarto: ");
-                int numeroQuarto = int.Parse(Console.ReadLine());
-                Aluguel aluguel = new Aluguel(hospede, email, numeroQuarto);
-                if (!aluguel.QuartoOcupado(numeroQuarto, alugueis))
+                try
                 {
-                    alugueis[i - 1] = aluguel;
+                    Console.Clear();
+                    Console.Write("Qual a quantidade de quartos disponíveis para alugar? ");
+                    int qteQuartos = int.Parse(Console.ReadLine());
+                    Aluguel[] alugueis = new Aluguel[qteQuartos];
+                    Console.WriteLine();
+                    Console.Write("Quantos aluguéis serão registrados (1 a " + qteQuartos + ")? ");
+                    int qteAlugueis = int.Parse(Console.ReadLine());
+                    for (int i = 1; i <= qteAlugueis; i++)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Dados do " + i + "° aluguel:");
+                        Console.Write("Nome: ");
+                        string hospede = Console.ReadLine();
+                        Console.Write("Email: ");
+                        string email = Console.ReadLine();
+                        Console.Write("Quarto: ");
+                        int numeroQuarto = int.Parse(Console.ReadLine());
+                        Aluguel aluguel = new Aluguel(hospede, email, numeroQuarto);
+                        if (!aluguel.QuartoOcupado(numeroQuarto, alugueis))
+                        {
+                            alugueis[i - 1] = aluguel;
 
+                        }
+                        else
+                        {
+                            Console.WriteLine("O quarto escolhido encontra-se ocupado!");
+                            i = i - 1;
+                        }
+                    }
+
+                    Console.WriteLine();
+                    Console.WriteLine("Quartos ocupados:");
+                    for (int i = 0; i < qteQuartos; i++)
+                    {
+                        if (alugueis[i] != null)
+                        {
+                            Console.WriteLine(alugueis[i]);
+                        }
+                    }
+                    k++;
                 }
-                else
+                catch (Exception e)
                 {
-                    Console.WriteLine("O quarto escolhido encontra-se ocupado!");
-                    i = i - 1;
+                    Console.WriteLine("Erro: " + e.Message);
+                    Console.WriteLine("Digite qualquer tecla para tentar novamente.");
+                    Console.ReadKey();
                 }
             }
+            
+            
 
-            Console.WriteLine();
-            Console.WriteLine("Quartos ocupados:");
-            for (int i = 0; i < qteQuartos; i++)
-            {
-                if (alugueis[i] != null)
-                {
-                    Console.WriteLine(alugueis[i]);
-                }
-            }
+            
 
             Console.ReadLine();
         }
